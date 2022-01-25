16890ef74eedd6889d19c7e7d77b7c4f6905d138
adrianetodesign <adrianetodesign@gmail.com>
2022-01-25 00:34:37 +0000

    Completed first part of implementing new URL form.

--- express_server.js
+++ express_server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,6 +26,15 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -43,3 +57,5 @@ app.get("/set", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+console.log(generateRandomString());
