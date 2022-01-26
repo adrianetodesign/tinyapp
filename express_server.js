3dcdb44af1c1affa451f198c89307d63765c60ba
adrianetodesign <adrianetodesign@gmail.com>
2022-01-26 00:48:24 +0000

    Created login/logout functionality, as well as the post methods.

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -25,7 +27,7 @@ app.get("/urls.json", (req, res) => {
 
 // urls index page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render("urls_index", templateVars);
 })
 
@@ -43,26 +45,45 @@ app.post("/urls", (req, res) => {
 
 // Page for each individual url
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL], 
+    username: req.cookies['username'] };
   res.render("urls_show", templateVars);
 });
 
-// Redirect back to /urls after delet button
+// Deletes the short url created
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect(`/urls`);
 });
 
+// Edit url post
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.newLongURL;
   res.redirect("/urls/");
-})
+});
+
+// Login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect("/urls");
+});
+
+// Logout
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
 
 // Redirect to actual URL of our short url.
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
+  if (longURL === undefined) {
+    res.send(302);
+  }
   res.redirect(longURL);
 });
 
